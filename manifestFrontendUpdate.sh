afd7c8209517a3756132403edf13fadfdfbc9c76
Brandon Jung <jwcnet93@gmail.com>
2022-11-04 19:53:09 +0900

    Update manifestFrontendUpdate.sh

--- manifestFrontendUpdate.sh
+++ manifestFrontendUpdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "s/Frontend*/Frontend$1/g" front-end-deployment-service.yaml
+sed -i "s/goormbank:Frontend*/goormbank:Frontend$1/g" front-end-deployment-service.yaml
 git add front-end-deployment-service.yaml
 git commit -m "[UPDATE] Frontend$1 image versioning"
 git push origin main
